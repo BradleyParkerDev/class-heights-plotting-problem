1903317712fab5edc337fbb1cb49b9f3ded7cfcd
Bradley Parker <Bradleyscottparker@outlook.com>
2023-10-27 19:06:27 -0500

    finished problem 3

--- class-heights-plotting-problem.py
+++ class-heights-plotting-problem.py
@@ -25,28 +25,39 @@ def convert_height(height_str):
     total_inches = feet * 12 + inches
     return total_inches * 2.54
 df['Height'] = df['Height'].apply(convert_height)
-df
+mean_class_height = df['Height'].mean()
+df['Mean_Height'] = df['Height'].apply(lambda x: mean_class_height)
+df['Difference_From_Mean'] = df['Height'].apply(lambda x: mean_class_height - x)
 
+
+# Separate DFs for Male and Female Created
+x = [i for i in range(0,80)]  # Use the index of the DataFrame as 'x'
 female_data_frame = df[df['Gender']=='female']
+
 female_data_frame=female_data_frame.sort_values(by='Height')
+female_data_frame.index = x
+
 male_data_frame = df[df['Gender']=='male']
 male_data_frame = male_data_frame.sort_values(by='Height')
-x = [i for i in range(0,80)]  # Use the index of the DataFrame as 'x'
-female_data_frame.index = x
-print(female_data_frame)
 male_data_frame.index = x
-print(male_data_frame)
 
+print(female_data_frame)
+print(male_data_frame)
+print()
+print(f' Mean Class Height: {mean_class_height}')
+print()
+print(df)
 
 
 # ///////////////////////////////////////////////////////////////
 # Lineplot
 # ///////////////////////////////////////////////////////////////
+# 1) Plot the heights in cm as a line plot in matplotlib
 # Female / Male Line Plots 
 plt.plot(x, female_data_frame['Height'], label='Female', color='red')
 plt.plot(x, male_data_frame['Height'], label='Male', color='blue')
 
-# Add labels and a legend
+# labels and a legend
 plt.xlabel('Number of Students')
 plt.ylabel('Heights (cm)')
 plt.legend()
@@ -61,22 +72,51 @@ plt.show()
 # ///////////////////////////////////////////////////////////////
 # Scatterplot
 # ///////////////////////////////////////////////////////////////
+# 2) Plot the average height of males against females in the class using a scatterplot in either seaborn or matplotlib
 # Create a single plot
 plt.figure(figsize=(6, 6))
 # Female / Male Scatter Points
-plt.scatter(female_data_frame['Gender'],female_data_frame['Height'], label='Individual', color='grey', marker='o', alpha=0.5)
-plt.scatter(male_data_frame['Gender'],male_data_frame['Height'], color='grey', marker='o', alpha=0.5)
+plt.scatter(female_data_frame['Gender'],female_data_frame['Height'], label='Individual', color='grey', marker='o', alpha=1)
+plt.scatter(male_data_frame['Gender'],male_data_frame['Height'], color='grey', marker='o', alpha=1)
 
 # Average Scatter Points
-plt.scatter('Female',female_data_frame['Height'].mean(), label='Average', color='red', marker='o', alpha=0.5,  s=100)
-plt.scatter('Male',male_data_frame['Height'].mean(), color='red', marker='o', alpha=0.5,  s=100)
+plt.scatter('Female',female_data_frame['Height'].mean(), label='Average', color='red', marker='o', alpha=1,  s=100)
+plt.scatter('Male',male_data_frame['Height'].mean(), color='red', marker='o', alpha=1,  s=100)
 
-# Add labels and a legend
+# labels and a legend
 plt.xlabel('Genders')
 plt.ylabel('Height (cm)')
 plt.legend()
 
 # Show the plot
 plt.title('Class Heights (Gender)')
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.show()
+
+
+
+# ///////////////////////////////////////////////////////////////
+# Scatterplot w/ Line  Class Heights vs Mean
+# ///////////////////////////////////////////////////////////////
+# 3) Create a separate column in the data frame to hold the difference from the mean height. 
+# Plot the difference from the mean against the heights on the same plot.
+
+# Create a single plot
+plt.figure(figsize=(6, 6))
+
+# Scatter plot for the difference from the mean
+plt.scatter(x,female_data_frame['Height'], label='Female', color='red', marker='o', alpha=1)
+plt.scatter(x, male_data_frame['Height'], label='Male', color='blue', marker='o', alpha=1)
+
+# Add a line for the mean difference from the mean
+plt.axhline(mean_class_height, color='green', linestyle='-', label='Mean Height')
+
+# Add labels and a legend
+plt.xlabel('Number of Students')
+plt.ylabel('Height (cm)')
+plt.legend()
+
+# Show the plot
+plt.title('Mean vs. Heights')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.show()
